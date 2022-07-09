90e2095b122252d10d2ec37c0ca0ec3c7d6ee2a7
freelancerbanna <hasanulhaquebanna@gmail.com>
2022-07-10 01:24:41 +0600

    mutation is done

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -13,6 +13,34 @@ module.exports = {
     },
   },
   Mutation: {
-    // createRecipe:async()
+    createRecipe: async (_, { recipeInput: { name, description } }) => {
+      const createRecipe = new Recipe({
+        name: name,
+        description: description,
+        createdAt: new Date().toISOString(),
+        thumbsUp: 0,
+        thumbsDown: 0,
+      });
+
+      const res = await createRecipe.save();
+      return {
+        id: res._id,
+        ...res._doc,
+      };
+    },
+    deleteRecipe: async (_, { ID }) => {
+      const wasDeleted = (await Recipe.deleteOne()).deletedCount;
+      return wasDeleted;
+    },
+    editRecipe: async (_, { ID, recieInput: { name, descrition } }) => {
+      const wasEdited = (
+        await Recipe.updateOne(
+          { _id: ID },
+          { $set: { name: name, description: description } },
+          { new: true }
+        )
+      ).modifiedCount;
+      return wasEdited;
+    },
   },
 };
