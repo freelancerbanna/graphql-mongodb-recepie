65e5fc5de96fb66c55fa7b213581a4391a17688f
freelancerbanna <hasanulhaquebanna@gmail.com>
2022-07-09 14:39:00 +0600

    typeDefs confgure

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
 const { ApolloServer } = require("apollo-server-express");
-const {
-  ApolloServerPluginLandingPageGraphQLPlayground,
-} = require("apollo-server-core");
 
-const { typeDefs, resolvers } = require("./schema");
+const { DB } = process.env.DB;
+
+const typeDefs = require("./graphql/typeDefs");
+const resolvers = require("./graphql/resolvers");
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  //   plugins: ApolloServerPluginLandingPageGraphQLPlayground,
 });
+
+mongoose
+  .connect(DB, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("Connected to Mongodb");
+    return server.listen({ port: 4000 });
+  })
+  .then((res) => console.log(`Server is running on port ${res.url}`));
---
+++ models/recipe.js
@@ -0,0 +1,11 @@
+const { Schema, modle } = require("mongoose");
+
+const recipeSchema = new Schema({
+  name: String,
+  description: String,
+  createdAt: String,
+  thumbsUp: Number,
+  thumbsDown: Number,
+});
+
+module.exports = model("Recipe", recipeSchema);
