dc2b1923396e23460a687e89eaf161245a94f8ad
freelancerbanna <hasanulhaquebanna@gmail.com>
2022-07-09 16:57:57 +0600

    apollo server started successfully

--- index.js
+++ index.js
@@ -1,21 +1,30 @@
 require("dotenv").config();
+const express = require("express");
 const mongoose = require("mongoose");
 const { ApolloServer } = require("apollo-server-express");
 
-const { DB } = process.env;
+const { DB, PORT } = process.env;
 
 const typeDefs = require("./graphql/typeDefs");
 const resolvers = require("./graphql/resolvers");
 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-});
+async function startApolloServer() {
+  const app = express();
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+  });
+  await server.start();
 
+  server.applyMiddleware({ app });
+
+  await new Promise((resolve) => app.listen({ port: PORT }, resolve));
+  console.log(`🚀 Server ready at ${PORT}${server.graphqlPath}`);
+  return { server, app };
+}
 mongoose
   .connect(DB, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log("Connected to Mongodb");
-    return server.listen({ port: 4000 });
-  })
-  .then((res) => console.log(`Server is running on port ${res.url}`));
+    startApolloServer();
+  });
